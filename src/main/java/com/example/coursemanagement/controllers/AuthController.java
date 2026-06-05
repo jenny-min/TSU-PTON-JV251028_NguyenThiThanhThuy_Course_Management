b2b44638a394e17a0420b767459475e952dba051
jenny-min <jennynguyen.92.92@gmail.com>
2026-06-05 12:50:18 +0900

    fix

--- src/main/java/com/example/coursemanagement/controllers/AuthController.java
+++ src/main/java/com/example/coursemanagement/controllers/AuthController.java
@@ -3,9 +3,7 @@ package com.example.coursemanagement.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
